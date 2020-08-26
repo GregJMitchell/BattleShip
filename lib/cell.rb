e0215708c532cf5bf7fc21e3f5b19df0d75fb367
Robert Heath <robert.b.heathii@gmail.com>
2020-08-26 16:17:54 -0500

    Create and test board class

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,45 +1,20 @@
-require './lib/ship'
-
 class Cell
-  attr_reader :coordinate, :ship
+
+attr_reader :coordinate, :ship
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = ship
   end
 
   def empty?
-    if @ship.nil?
-      true
+    if @ship == nil
+     true
     else
-      false
+     false
     end
   end
 
   def place_ship(new_ship)
     @ship = new_ship
-  end
-
-  def fired_upon?
-    if @ship.health < @ship.length
-      true
-    else
-      false
-    end
-  end
-
-  def fire_upon
-    @ship.hit
-  end
-
-  def render(player = nil)
-    if @ship.sunk? == true
-      'X'
-    elsif fired_upon? == true
-      'H'
-    elsif player == nil
-      '.'
-    else
-      'S'
-    end
   end
 end
