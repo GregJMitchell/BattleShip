69ce1f563dea85c9fa95f93ec5e6ec666a945436
Robert Heath <robert.b.heathii@gmail.com>
2020-08-26 16:49:12 -0500

    Code and test valid_coordinate method

--- test/board_test.rb
+++ test/board_test.rb
@@ -9,4 +9,17 @@ class BoardTest < Minitest::Test
     board = Board.new
     assert_instance_of Board, board
   end
+
+  def test_hash_exists
+    board = Board.new
+    board.cells
+    assert_equal 16, board.cells.keys.length
+    assert_equal 16, board.cells.values.length
+  end
+
+  def test_it_can_validate_coordinates
+    board = Board.new
+    assert_equal true, board.valid_coordinate?("A1")
+    assert_equal false, board.valid_coordinate?("A5")
+  end
 end
