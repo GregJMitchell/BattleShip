6c79ea18166342dd5d405aac646e12a70ac8b32d
Greg Mitchell <g2mitchell3003@gmail.com>
2020-08-25 19:16:37 -0600

    Add fired upon method and test to cell class and test file

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,20 +1,27 @@
 class Cell
-
-attr_reader :coordinate, :ship
+  attr_reader :coordinate, :ship
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = ship
   end
 
   def empty?
-    if @ship == nil
-     true
+    if @ship.nil?
+      true
     else
-     false
+      false
     end
   end
 
   def place_ship(new_ship)
     @ship = new_ship
+  end
+
+  def fired_upon?
+    if @ship.health < @ship.length
+      true
+    else
+      false
+    end
   end
 end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -28,4 +28,16 @@ class CellTest < Minitest::Test
     assert_equal "Cruiser", cell.ship.name
     assert_equal false, cell.empty?
   end
+
+  def test_it_can_be_fired_upon?
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+
+    assert_equal false, cell.fired_upon?
+
+    cell.fire_upon
+    
+    assert_equal 2, cell.ship.health
+  end
 end
