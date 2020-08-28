71184dc9210bb988dfce797441c36aee9a22b6e9
Greg Mitchell <g2mitchell3003@gmail.com>
2020-08-27 19:28:02 -0600

    Create tests and method for placing ships

--- lib/board.rb
+++ lib/board.rb
@@ -6,22 +6,22 @@ class Board
 
   def initialize
     @cells = {
-      "A1" => Cell.new("A1"),
-      "A2" => Cell.new("A2"),
-      "A3" => Cell.new("A3"),
-      "A4" => Cell.new("A4"),
-      "B1" => Cell.new("B1"),
-      "B2" => Cell.new("B2"),
-      "B3" => Cell.new("B3"),
-      "B4" => Cell.new("B4"),
-      "C1" => Cell.new("C1"),
-      "C2" => Cell.new("C2"),
-      "C3" => Cell.new("C3"),
-      "C4" => Cell.new("C4"),
-      "D1" => Cell.new("D1"),
-      "D2" => Cell.new("D2"),
-      "D3" => Cell.new("D3"),
-      "D4" => Cell.new("D4")
+      'A1' => Cell.new('A1'),
+      'A2' => Cell.new('A2'),
+      'A3' => Cell.new('A3'),
+      'A4' => Cell.new('A4'),
+      'B1' => Cell.new('B1'),
+      'B2' => Cell.new('B2'),
+      'B3' => Cell.new('B3'),
+      'B4' => Cell.new('B4'),
+      'C1' => Cell.new('C1'),
+      'C2' => Cell.new('C2'),
+      'C3' => Cell.new('C3'),
+      'C4' => Cell.new('C4'),
+      'D1' => Cell.new('D1'),
+      'D2' => Cell.new('D2'),
+      'D3' => Cell.new('D3'),
+      'D4' => Cell.new('D4')
     }
   end
 
@@ -31,16 +31,28 @@ class Board
 
   def valid_placement?(ship, placement)
     return false if ship.length != placement.length
-    
-    number_array, letter_array = [], []
+
+    number_array = []
+    letter_array = []
     placement.each do |cell|
-      return false if !valid_coordinate?(cell)
+      return false unless valid_coordinate?(cell)
 
       letter_array << cell[0]
       number_array << cell[1]
     end
     return true if letter_array.uniq.length == 1 && number_array == (number_array[0]..number_array[-1]).to_a
     return true if number_array.uniq.length == 1 && letter_array == (letter_array[0]..letter_array[-1]).to_a
+
     false
+  end
+
+  def place(ship, placement)
+    if valid_placement?(ship, placement) == false
+      false
+    else
+      placement.each do |cell|
+        @cells[cell].place_ship(ship)
+      end
+    end
   end
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -2,9 +2,10 @@ require './lib/board'
 require 'pry'
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/cell'
+require './lib/ship'
 
 class BoardTest < Minitest::Test
-
   def test_it_exists
     board = Board.new
     assert_instance_of Board, board
@@ -19,20 +20,34 @@ class BoardTest < Minitest::Test
 
   def test_it_can_validate_coordinates
     board = Board.new
-    assert_equal true, board.valid_coordinate?("A1")
-    assert_equal false, board.valid_coordinate?("A5")
+    assert_equal true, board.valid_coordinate?('A1')
+    assert_equal false, board.valid_coordinate?('A5')
   end
 
   def test_it_can_validate_placement
     board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
-    assert_equal true, board.valid_placement?(submarine,["A1", "A2"])
-    assert_equal false, board.valid_placement?(submarine, ["A1", "A3"])
-    assert_equal false, board.valid_placement?(cruiser, ["E1", "E2", "E3"])
-    assert_equal true, board.valid_placement?(cruiser, ["A1", "B1", "C1"])
-    assert_equal false, board.valid_placement?(cruiser, ["A2", "B1", "C1"])
-    assert_equal false, board.valid_placement?(cruiser, ["A5", "B5", "C5"])
-    assert_equal false, board.valid_placement?(cruiser, ["A1", "B1"])
+    cruiser = Ship.new('Cruiser', 3)
+    submarine = Ship.new('Submarine', 2)
+    assert_equal true, board.valid_placement?(submarine, %w[A1 A2])
+    assert_equal false, board.valid_placement?(submarine, %w[A1 A3])
+    assert_equal false, board.valid_placement?(cruiser, %w[E1 E2 E3])
+    assert_equal true, board.valid_placement?(cruiser, %w[A1 B1 C1])
+    assert_equal false, board.valid_placement?(cruiser, %w[A2 B1 C1])
+    assert_equal false, board.valid_placement?(cruiser, %w[A5 B5 C5])
+    assert_equal false, board.valid_placement?(cruiser, %w[A1 B1])
+  end
+
+  def test_it_can_place_ships
+    board = Board.new
+    cruiser = Ship.new('Cruiser', 3)
+    board.place(cruiser, %w[A1 A2 A3])
+    cell_1 = board.cells['A1']
+    cell_2 = board.cells['A2']
+    cell_3 = board.cells['A3']
+
+    assert_equal cruiser, cell_1.ship
+    assert_equal cruiser, cell_2.ship
+    assert_equal cruiser, cell_3.ship
+    assert_equal true, cell_3.ship == cell_2.ship
   end
 end
